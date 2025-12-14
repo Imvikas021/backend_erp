a1e8881ed94dffe686c7f8350660fb11a2ee9bf9
imvikas021 <141994868+Imvikas021@users.noreply.github.com>
2025-12-14 18:14:15 +0530

    fix api

--- App.js
+++ App.js
@@ -5,10 +5,7 @@ const app = express();
 const cors = require("cors");
 
 const corsOptions = {
-    origin: [
-        "http://localhost:3000",
-        "https://frontenderp-production.up.railway.app"
-    ],
+    origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeades: ["Content-Type", "Authorization"],
     Credential: true
