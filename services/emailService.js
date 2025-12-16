21d55471c0c8e7dab5ffcee43b3d72145e77e937
imvikas021 <141994868+Imvikas021@users.noreply.github.com>
2025-12-16 14:36:24 +0530

    update crash

--- services/emailService.js
+++ services/emailService.js
@@ -1,9 +1,8 @@
 const sgMail = require("@sendgrid/mail");
-const Module = require("node:module");
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-exports.sendMail = async ({ to, subject, message, attachments}) => {
+const sendMail = async ({ to, subject, message, attachments}) => {
   const msg = {
     to,
     from: process.env.SENDGRID_FROM_EMAIL,
